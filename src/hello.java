6109cc89a52c2969b32166c5e7b0179fdd3cd874
jjgdelgado <jjgalandelgado@gmail.com>
2020-03-30 16:31:14 -0500

    Add files via upload

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+   public static void main(String []args) {
+      System.out.println("hello world");
+      int num = method.add();
+      System.out.println("the number = " + num);
+   }
+   public static int method() {
+      return 2*3;
+   }
+}
---
+++ test/hellotest.java
@@ -0,0 +1,20 @@
+//package hello;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class hellotest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testmethod() {   
+	string first="one";
+	string second="one"     
+        assertEquals(first,second);
+    }
+
+
+}
